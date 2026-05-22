2fd386a1c30934e5df72dbf1fc592c063f3e20f3
Ucastle <skaska007@naver.com>
2026-05-22 22:54:37 +0900

    feat: 대시보드 생성 모달 구현 및 연결

--- src/app/myDashboard/page.tsx
+++ src/app/myDashboard/page.tsx
@@ -3,15 +3,30 @@
 import Button from '@/components/ui/Button';
 import InvitedDashboard from '@/domains/myDashboard/components/InvitedDashboardList';
 import MyDashboardButton from '@/domains/myDashboard/components/MyDashboardButton';
+import PlusMark from '@/components/icons/PlusMark';
+import CreateDashboardModal from '@/domains/myDashboard/components/CreateDashboardModal';
+
+import { useState } from 'react';
 
 export default function Page() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div>
       <main className="mt-10 ml-6 md:ml-9 lg:ml-10 flex w-full flex-col gap-8 lg:max-w-240">
         <section className="grid w-65 md:w-126 lg:w-full grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3">
-          <Button variant="add" className="h-14.5 w-full md:h-17 lg:h-17.5">
-            <p className="text-sm font-semibold md:text-base">새로운 대시보드</p>
+          <Button
+            onClick={() => setIsOpen(true)}
+            variant="add"
+            className="h-14.5 w-full md:h-17 lg:h-17.5"
+          >
+            <div className="flex items-center gap-2">
+              <p className="text-sm font-semibold md:text-base">새로운 대시보드</p>
+              <PlusMark size={12} thickness={1} />
+            </div>
           </Button>
+          <CreateDashboardModal open={isOpen} onClose={() => setIsOpen(false)} />
+
           <MyDashboardButton
             className="h-14.5 w-full md:h-17 lg:h-17.5"
             dashboardName="2분기 계획"

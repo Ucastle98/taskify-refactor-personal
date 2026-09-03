b1fb50ce3cdb22a1bf8e3ee4213126c6844ab94a
Ucastle <skaska007@naver.com>
2026-09-03 17:45:36 +0900

    feat: dashboard api 필드 생성 및 리팩토링

--- src/services/dashboard.ts
+++ src/services/dashboard.ts
@@ -1,5 +1,10 @@
 import { api } from '@/lib/api';
-import type { Dashboard, DashboardListResponse } from '@/types/dashboard';
+import type {
+  Dashboard,
+  DashboardInvite,
+  DashboardInviteListResponse,
+  DashboardListResponse,
+} from '@/types/dashboard';
 
 const teamId = process.env.NEXT_PUBLIC_TEAM_ID;
 
@@ -17,6 +22,10 @@ export interface CreateDashboardRequest {
   color: string;
 }
 
+export interface InviteDashboardRequest {
+  email: string;
+}
+
 export const createDashboard = async (data: CreateDashboardRequest) => {
   const response = await api.post<Dashboard>(`/dashboards`, data);
   return response.data;
@@ -25,4 +34,29 @@ export const createDashboard = async (data: CreateDashboardRequest) => {
 export const getDashboard = async (dashboardId: number) => {
   const response = await api.get<Dashboard>(`/dashboards/${dashboardId}`);
   return response.data;
+};
+
+export const modifyDashboard = async (dashboardId: number, data: CreateDashboardRequest) => {
+  const response = await api.put<Dashboard>(`/dashboards/${dashboardId}`, data);
+  return response.data;
+};
+
+export const deleteDashboard = async (dashboardId: number) => {
+  await api.delete(`/dashboards/${dashboardId}`);
+};
+
+export const inviteDashboard = async (dashboardId: number, data: InviteDashboardRequest) => {
+  const response = await api.post<DashboardInvite>(`/dashboards/${dashboardId}/invitations`, data);
+  return response.data;
+};
+
+export const getInviteDashboards = async (dashboardId: number) => {
+  const response = await api.get<DashboardInviteListResponse>(
+    `/dashboards/${dashboardId}/invitations`,
+  );
+  return response.data;
+};
+
+export const cancelInvite = async (dashboardId: number, invitationId: number) => {
+  await api.delete(`/dashboards/${dashboardId}/invitations/${invitationId}`);
 };
